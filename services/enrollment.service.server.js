8b2c9fb06f780b64930e82c39f7a6ab97fd047ce
parshva45 <parshva45@gmail.com>
2018-06-18 17:05:50 -0400

    updated enrollment service

--- services/enrollment.service.server.js
+++ services/enrollment.service.server.js
@@ -1,17 +1,38 @@
 module.exports = function (app) {
 
-  app.post('/api/enroll/student/:studentId/section/:sectionId',
-    enrollStudentInSection);
+  app.post('/api/section/:sectionId/enrollment', enrollStudentInSection);
+  app.get('/api/student/section', findEnrolledSectionsForStudent);
 
   var enrollmentModel = require('../models/enrollment/enrollment.model.server');
 
-  function enrollStudentInSection(req, res) {
+  function findEnrolledSectionsForStudent(req, res) {
+    var currentUser = req.session.currentUser;
+    var studentId = currentUser._id;
     enrollmentModel
-      .enrollStudentInSection(
-        req.params.studentId,
-        req.params.sectionId)
-      .then(function (enrollment) {
-        res.json(enrollment);
+      .findSectionsForStudent(studentId)
+      .then(function (enrollments) {
+        res.json(enrollments);
       });
   }
+
+  function enrollStudentInSection(req, res) {
+    var sectionId = req.params.sectionId;
+    var currentUser = req.session.currentUser;
+    var studentId = currentUser._id;
+    var enrollment = {
+      student: studentId,
+      section: sectionId
+    };
+
+    sectionModel
+      .decrementSectionSeats(sectionId)
+      .then(function () {
+        return enrollmentModel
+          .enrollStudentInSection(enrollment)
+      })
+      .then(function (enrollment) {
+        res.json(enrollment);
+      })
+  }
+
 };
