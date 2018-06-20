89e1ac623c7e32f5a768d44edff6d4b6b3e36e79
parshva45 <parshva45@gmail.com>
2018-06-20 19:56:40 -0400

    updating server.js

--- server.js
+++ server.js
@@ -10,8 +10,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin",
-    "http://localhost:4200");
+
+  var allowedOrigins = [
+    "http://localhost:4200",
+    "https://assignment5-nodejs-app.herokuapp.com"
+  ];
+  var origin = req.headers.origin;
+  if(allowedOrigins.indexOf(origin) > -1){
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+
   res.header("Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods",
